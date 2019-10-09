d412bb7aaf0333adbfbe91ffc4ccc5b8b96f599f
chuxu <s17chucc@163.com>
2019-10-09 16:04:25 +0800

    update

--- assets/js/62.989b81ac.js
+++ assets/js/62.989b81ac.js
@@ -1,4 +1,1 @@
-(window.webpackJsonp = window.webpackJsonp || []).push([
-  [62],
-  { 56: function(n, w, o) {} }
-]);
+(window.webpackJsonp=window.webpackJsonp||[]).push([[62],{56:function(n,w,o){}}]);
